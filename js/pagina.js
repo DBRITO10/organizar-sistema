936df360c43c21dd03fc561fd0aa9c11fbb4e688
Douglas Brito Da Rocha <oceaniluminat@gmail.com>
2026-03-08 13:56:36 -0300

    Create pagina.js

---
+++ js/pagina.js
@@ -0,0 +1,26 @@
+import { db, auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async user => {
+    if (user) {
+        try {
+            const userDoc = await getDoc(doc(db, "users", user.uid));
+            if (!userDoc.exists()) { window.location.href = "first_login.html"; return; }
+
+            const userData = userDoc.data();
+            const userRole = (userData.role || "leitor").toLowerCase();
+            const nomeComp = userData.nomeCompleto || "Usuário";
+
+            document.getElementById("userDisplay").innerText = nomeComp;
+
+            if (userRole === "admin") {
+                document.getElementById("btnGestao").style.display = "flex";
+            }
+        } catch (e) { console.error(e); }
+    } else { window.location.href = "index.html"; }
+});
+
+document.getElementById("btnLogout").onclick = () => {
+    auth.signOut().then(() => { window.location.href = "index.html"; });
+};
